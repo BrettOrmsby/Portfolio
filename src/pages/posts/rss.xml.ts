f1f56a87c5e5764079f6feb9302d762e84b46310
BrettOrmsby <brettwormsby@outlook.com>
2023-06-18 11:34:47 -0400

    fix: update page links

--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: "Brett Ormsby | Blog",
     description: "My articles about the projects I work on.",
-    site: "github.com",
+    site: "https://brettormsby.github.io/portfolio/",
     items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
     customData: `<language>en-ca</language>`,
   });
