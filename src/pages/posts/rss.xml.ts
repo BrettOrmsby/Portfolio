d82178e6d267fe68df3fde344da2165e7b696e02
BrettOrmsby <brettwormsby@outlook.com>
2024-01-18 17:45:38 -0500

    refractor: move to using content collections

--- src/pages/posts/rss.xml.ts
+++ src/pages/posts/rss.xml.ts
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
-export async function get() {
+export async function GET() {
+  const posts = await getCollection("posts");
   return rss({
     title: "Brett Ormsby | Blog",
     description: "My articles about the projects I work on.",
     site: "https://brettormsby.github.io/portfolio/",
-    items: await pagesGlobToRssItems(import.meta.glob("./*.mdx")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`
+    })),
     customData: `<language>en-ca</language>`
   });
 }
